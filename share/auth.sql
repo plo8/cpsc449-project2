c792182bf26afe8e8b82e46ce9c2a402d01ec195
Debdyuti <das.debdyuti01@gmail.com>
2022-11-12 20:00:56 -0800

    Added changes in sql auth

--- share/auth.sql
+++ share/auth.sql
@@ -3,8 +3,8 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS "userData";
 CREATE TABLE IF NOT EXISTS "userData" (	
-	"username"	TEXT UNIQUE CHECK(username != '' OR username!= null),
-	"password"	TEXT CHECK(username != '' OR username!= null),
+	"username"	TEXT UNIQUE CHECK(length(username)>0 OR username!= null),
+	"password"	TEXT CHECK(length(password)>0 OR password!= null),
 	PRIMARY KEY("username")
 );
 COMMIT;
