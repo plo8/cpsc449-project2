d76ccd4f6a50b4eac93abe6040c127e087f1a0c1
Debdyuti <das.debdyuti01@gmail.com>
2022-11-12 19:49:34 -0800

    Testing empty or null values

--- share/auth.sql
+++ share/auth.sql
@@ -3,8 +3,8 @@ BEGIN TRANSACTION;
 
 DROP TABLE IF EXISTS "userData";
 CREATE TABLE IF NOT EXISTS "userData" (	
-	"username"	TEXT UNIQUE NOT NULL,
-	"password"	TEXT,
+	"username"	TEXT UNIQUE CHECK(username != '' OR username!= null),
+	"password"	TEXT CHECK(username != '' OR username!= null),
 	PRIMARY KEY("username")
 );
 COMMIT;
